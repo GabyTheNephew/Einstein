d38935b042716e7b354c69ce22474148e2199a45
georgee116 <georgestanga39@gmail.com>
2026-03-06 09:20:55 +0200

    regula 7 - 10

--- swish.pl
+++ swish.pl
@@ -7,7 +7,7 @@ langa(X, Y, Lista) :- la_dreapta(X, Y, Lista).
 langa(X, Y, Lista) :- la_dreapta(Y, X, Lista).
 
 
-strucutrare(Case) :-
+structurare(Case) :-
     Case = [
         casa(_, _, _, _, _),
         casa(_, _, _, _, _),
@@ -20,7 +20,11 @@ strucutrare(Case) :-
     member(casa(_, spaniol, _, _, caine), Case),
     member(casa(verde, _, cafea, _, _), Case),
     member(casa(_, ucrainian, ceai, _, _), Case),
-    la_dreapta(casa(verde, _, _, _, _), casa(alba, _, _, _, _), Case),
+    la_dreapta(casa(alba, _, _, _, _), casa(verde, _, _, _, _), Case),
     member(casa(_, _, _, old_gold, melci), Case),
+    member(casa(galbena, _, _, kools, _), Case),
+    Case = [_, _, casa(_, _, lapte, _, _), _, _],
+    Case = [casa(_, norvegian, _, _, _),_, _, _, _],
+    langa(casa(_, _, _, chesterfields, _), casa(_, _, _, _, vulpe), Case),
 
     true.
