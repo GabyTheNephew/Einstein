afe14be64558622871b7c70599bfe485d9328694
LaurentiuPorfireanu <laurporfireanu@gmail.com>
2026-03-06 08:27:14 +0200

    Partea 1.

---
+++ swish.pl
@@ -0,0 +1,22 @@
+la_dreapta(Y, X, [Y, X, _, _, _]).
+la_dreapta(Y, X, [_, Y, X, _, _]).
+la_dreapta(Y, X, [_, _, Y, X, _]).
+la_dreapta(Y, X, [_, _, _, Y, X]).
+
+langa(X, Y, Lista) :- la_dreapta(X, Y, Lista).
+langa(X, Y, Lista) :- la_dreapta(Y, X, Lista).
+
+
+strucutrare(Case) :-
+    Case = [
+        casa(_, _, _, _, _),
+        casa(_, _, _, _, _),
+        casa(_, _, _, _, _),
+        casa(_, _, _, _, _),
+        casa(_, _, _, _, _)
+    ],
+
+    member(casa(rosie, englez, _, _, _), Case),
+    member(casa(_, spaniol, _, _, caine), Case),
+    /%aiaic puneti codul type shit%/
+    true.
